187996dedae00e09f89a4542f7282e86e3a08d42
Sandeep Kumar <kumarsandeep110104@gmail.com>
2025-08-21 11:42:36 +0530

    Conflict1 has been added

--- f1.py
+++ f1.py
@@ -1,16 +1,1 @@
-def is_prime(n):
-    if n <= 1:
-        return False
-    if n == 2:
-        return True
-    if n % 2 == 0:
-        return False
-    for i in range(3, int(n ** 0.5) + 1, 2):
-        if n % i == 0:
-            return False
-    return True
-
-# Example usage: print all primes up to 100
-for num in range(1, 101):
-    if is_prime(num):
-        print(num)
+print("Conflict 1")
